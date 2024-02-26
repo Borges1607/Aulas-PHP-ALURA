f7cebc2b63b18cdb725cdc9de668b95eaef3a3f6
Borges1607 <felipedepauaborges@gmail.com>
2024-02-26 20:25:26 -0300

    aula7-comecando-heranca

--- banco.php
+++ banco.php
@@ -1,23 +1,32 @@
 <?php
 require_once 'src/Conta.php';
+require_once 'src/Endereco.php';
 require_once 'src/Titular.php';
 require_once 'src/CPF.php';
 
-$jonas = new Titular(new CPF('123.456.789-10'),'Jonas');
+$endereco = new Endereco('Petrópolis', 'um bairro', 'minha rua', '71B');
+
+$jonas = new Titular(new CPF('123.456.789-10'),'Jonas', $endereco);
 $primeiraConta = new Conta($jonas);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300);
 
-echo $primeiraConta->recuperaSaldo() . PHP_EOL;
-echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
-echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
+// echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+// echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
+// echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
+var_dump($primeiraConta);
 
-$patricia = new Titular(new CPF('456.123.789-10'),'Patricia');
+$patricia = new Titular(new CPF('456.123.789-10'),'Patricia', $endereco);
 $segundaConta = new Conta($patricia);
 
-echo $segundaConta->recuperaSaldo() . PHP_EOL;
-echo $segundaConta->recuperaCpfTitular() . PHP_EOL;
-echo $segundaConta->recuperaNomeTitular() . PHP_EOL;
+// echo $segundaConta->recuperaSaldo() . PHP_EOL;
+// echo $segundaConta->recuperaCpfTitular() . PHP_EOL;
+// echo $segundaConta->recuperaNomeTitular() . PHP_EOL;
+var_dump($segundaConta);
 
-$terceiraConta = new Conta(new Titular(new CPF('678.123.789-10'),'jonh'));
+$outroEndereco = new Endereco('A', 'b', 'c', 'd');
+
+$terceiraConta = new Conta(new Titular(new CPF('678.123.789-10'),'julho', $outroEndereco));
+var_dump($terceiraConta);
+
 echo Conta::recuperaNumeroDeContas();
--- src/Titular.php
+++ src/Titular.php
@@ -3,12 +3,14 @@ class Titular
 {
     private CPF $cpf;
     private string $nome;
+    private Endereco $endereco;
 
-    public function __construct(CPF $cpf, string $nome)
+    public function __construct(CPF $cpf, string $nome, Endereco $endereco)
     {
         $this->cpf = $cpf;
         $this->validaNomeTitular($nome);
         $this->nome = $nome;
+        $this->endereco = $endereco;
     }
 
     public function recuperaCpf(): string
@@ -19,6 +21,11 @@ class Titular
     public function recuperaNome(): string
     {
         return $this->nome;
+    }
+
+    public function recuperaEndereco(): Endereco
+    {
+        return $this->endereco;
     }
 
     private function validaNomeTitular(string $nomeTitular)
